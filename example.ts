fc9efd8325d5f15c58dca4e2384961580b46d150
Jake Teton-Landis <just.1.jake@gmail.com>
2019-04-12 12:41:01 -0700

    edit for clarity

--- example.ts
+++ example.ts
@@ -4,4 +4,26 @@ async function* generate() {
   yield 3
 }
 
+// AsyncIterableIterator<1 | 2 | 3>
 type Out = ReturnType<typeof generate>
+
+async function consume() {
+  const iterator = generate()
+  const yield1 = await iterator.next()
+  const yield2 = await iterator.next()
+  const yield3 = await iterator.next()
+  const yield4 = await iterator.next()
+
+  // IteratorResult<1 | 2 | 3>
+  type ExpectedYield4Type = typeof yield4
+  //  1 | 2 | 3
+  type ExpectedYield4ValueType = typeof yield4.value
+
+  console.log('yield4', yield4) // { value: undefined, done: true }
+  console.log('yiled4.value', yield4.value) // undefined
+  if (yield4.value === undefined) {
+    throw new Error('yield4.value type is 1 | 2 | 3, so value cannot be undefined')
+  }
+}
+
+consume()
